72a4bdcaa5419fc02d7e2b174a04fe09d2032ea9
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-28 22:57:28 +0200

    Add login api

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {register} from './user.service.js';
+import {register , login} from './user.service.js';
 const router = express.Router();
 
 router.post('/register',register);
+router.post('/login',login);
 
 
 
--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,5 +1,6 @@
 import {User} from '../../database/model/user.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req , res) =>{
     const {name , email , password , phone} = req.body;
@@ -9,4 +10,16 @@ export const register = async (req , res) =>{
     const newUser = await User.create({name , email , password:hashedPassword , phone});
     if(!newUser) return res.status(500).json({message:"Internal server error !"});
     res.status(201).json({message:"user created successfully",data: newUser});
+}
+
+export const login = async (req , res) =>{
+    const {email , password} = req.body;
+    const user = await User.findOne({email});
+    if(!user) return res.status(404).json({message:"this e-mail is incorrect !"});
+    const isUser = await bcrypt.compare(password , user.password);
+    if(!isUser) return res.status(404).json({message:"User password is incorrect"});
+    const secretKey = "DASHBOARD2026";
+    const userId = user._id;
+    const token = jwt.sign({userId},secretKey);
+    res.status(200).json({message:"success",data:{token}});    
 }
