31fe29dd1d4c875d02f957bd8f4d47be1fb4fdea
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-29 22:21:57 +0200

    Add getProductById function

--- modules/product/product.controller.js
+++ modules/product/product.controller.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllProducts} from "./product.service.js";
+import { getAllProducts , getProductbyId} from "./product.service.js";
 
 const router = Router();
 
 router.get('/',getAllProducts);
+router.get('/:id',getProductbyId);
 
 
 export default router;
--- modules/product/product.service.js
+++ modules/product/product.service.js
@@ -9,3 +9,10 @@ export const getAllProducts = async (req , res) =>{
     if(products.length <= 0) return res.status(400).json({message:"No products at this time"});
     res.status(200).json({message:"success",data:products});
 }
+
+export const getProductbyId = async (req , res) =>{
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    if(!product) return res.status(404).json({message:"this product is not exists"});
+    res.status(200).json({message:"success",data:product});
+}
