f135686cb164194d0c581fae9be61f174183fe18
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-31 00:06:22 +0200

    Add get specific category api

--- modules/category/category.service.js
+++ modules/category/category.service.js
@@ -24,3 +24,10 @@ export const getAllCategories = async (req , res) =>{
     if(categories.length <= 0) return res.status(400).json({message:"No categories at this time"});
     res.status(200).json({message:"success",data:{categories}});
 }
+
+export const getCategoryById = async (req , res) =>{
+    const {id} = req.params;
+    const category = await Category.findById(id);
+    if(!category) return res.status(404).json({message:"this category is not exists"});
+    res.status(200).json({message:"success",data:{category}});
+}
