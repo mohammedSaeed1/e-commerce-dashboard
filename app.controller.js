84d61e4bbf24e0d2ef4686587dec1fd10ef31b74
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-31 02:02:55 +0200

    Add Brand Router to the app

--- app.controller.js
+++ app.controller.js
@@ -3,6 +3,7 @@ import { databaseConnection } from './database/connetcion.js';
 import userRouter from './modules/user/user.controller.js';
 import productRouter from './modules/product/product.controller.js';
 import categoryRouter from './modules/category/category.controller.js';
+import brandRouter from './modules/brand/brand.controller.js';
 
 export const bootstrap = () => {
 
@@ -12,6 +13,7 @@ export const bootstrap = () => {
     app.use(`/api/v1/users`,userRouter);
     app.use(`/api/v1/products`,productRouter);
     app.use(`/api/v1/categories`,categoryRouter);
+    app.use(`/api/v1/brands`,brandRouter);
 
 
 
