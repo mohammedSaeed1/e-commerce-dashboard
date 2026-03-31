27f9435b51dca9057157ecf5eba99786f9fe69b9
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-04-01 01:22:52 +0200

    feat: edit delete product api

--- modules/product/product.service.js
+++ modules/product/product.service.js
@@ -4,42 +4,42 @@ import { Category } from "../../database/model/category.model.js";
 import slugify from "slugify";
 import { uploadImageToCloudinary } from "../../utilts/cloudinary.js";
 
-export const createProduct = async (req , res) =>{
-    const {title , description , price , category , brand } = req.body;
+export const createProduct = async (req, res) => {
+    const { title, description, price, category, brand } = req.body;
     let image;
-     const isCategory = await Category.findById(category);
-     if(!isCategory) return res.status(404).json({message:"Category is not found!!!"});
-      const isBrand = await Brand.findById(brand);
-     if(!isBrand) return res.status(404).json({message:"Brand is not found!!!"});
-      const slug = slugify(title,{lower:true});
-        if (req.file) {
+    const isCategory = await Category.findById(category);
+    if (!isCategory) return res.status(404).json({ message: "Category is not found!!!" });
+    const isBrand = await Brand.findById(brand);
+    if (!isBrand) return res.status(404).json({ message: "Brand is not found!!!" });
+    const slug = slugify(title, { lower: true });
+    if (req.file) {
         try {
-          image = await uploadImageToCloudinary(req.file.buffer, "products");
+            image = await uploadImageToCloudinary(req.file.buffer, "products");
         } catch (err) {
-          return res.status(500).json({ message: "Image upload failed" });
+            return res.status(500).json({ message: "Image upload failed" });
         }
-      }
-      const product = await Product.create({title , slug , price , description , category , brand , image});
-      if(!product) return res.status(500).json({message:"Internal server error"});
-      res.status(201).json({message:"success",data:{product}});
     }
+    const product = await Product.create({ title, slug, price, description, category, brand, image });
+    if (!product) return res.status(500).json({ message: "Internal server error" });
+    res.status(201).json({ message: "success", data: { product } });
+}
 
-export const getAllProducts = async (req , res) =>{
+export const getAllProducts = async (req, res) => {
     const products = await Product.find();
-    if(products.length <= 0) return res.status(400).json({message:"No products at this time"});
-    res.status(200).json({message:"success",data:products});
+    if (products.length <= 0) return res.status(400).json({ message: "No products at this time" });
+    res.status(200).json({ message: "success", data: products });
 }
 
-export const getProductById = async (req , res) =>{
-    const {id} = req.params;
+export const getProductById = async (req, res) => {
+    const { id } = req.params;
     const product = await Product.findById(id);
-    if(!product) return res.status(404).json({message:"this product is not exists"});
-    res.status(200).json({message:"success",data:product});
+    if (!product) return res.status(404).json({ message: "this product is not exists" });
+    res.status(200).json({ message: "success", data: product });
 }
 
-export const deleteProduct = async (req , res) =>{
-    const {id} = req.params;
-    const product = await Product.findById(id);
-    if(!product) return res.status(404).json({message:"this product is not exists"});
-    res.status(200).json({message:"success"});
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) return res.status(404).json({ message: "this product is not exists" });
+    res.status(200).json({ message: "success" });
 }
