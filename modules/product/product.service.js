20e9f72d6f46ebfba19cad783a7ec8bb9ff37783
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-04-01 02:43:24 +0200

    feat: add populate method to get all products api

--- modules/product/product.service.js
+++ modules/product/product.service.js
@@ -25,7 +25,7 @@ export const createProduct = async (req, res) => {
 }
 
 export const getAllProducts = async (req, res) => {
-    const products = await Product.find();
+    const products = await Product.find().populate('category brand');
     if (products.length <= 0) return res.status(400).json({ message: "No products at this time" });
     res.status(200).json({ message: "success", data: products });
 }
