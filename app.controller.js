62cb0c5d9980a63eb9888624840ce5e0d7e33211
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-04-05 15:29:01 +0200

    feat : Add Cart Schema

--- app.controller.js
+++ app.controller.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import { databaseConnection } from './database/connetcion.js';
-import userRouter from './modules/user/user.controller.js';
+import authRouter from './modules/auth/auth.controller.js';
 import productRouter from './modules/product/product.controller.js';
 import categoryRouter from './modules/category/category.controller.js';
 import brandRouter from './modules/brand/brand.controller.js';
+import userRouter from './modules/user/user.controller.js';
 
 export const bootstrap = () => {
 
     const app = express();
     app.use(express.json());
     databaseConnection();
-    app.use(`/api/v1/users`,userRouter);
+    app.use(`/api/v1/auth`,authRouter);
     app.use(`/api/v1/products`,productRouter);
     app.use(`/api/v1/categories`,categoryRouter);
     app.use(`/api/v1/brands`,brandRouter);
+    app.use(`/api/v1/users/profile`,userRouter);
 
 
 
