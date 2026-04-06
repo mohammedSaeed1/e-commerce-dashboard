1640f7716d89fa5ea0e173115427d00b4f2c0ebd
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-04-06 12:55:46 +0200

    feat: Add Order Schema

---
+++ database/model/order.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import { Product } from "./product.model";
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: true
+    },
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'products',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            }
+        }],
+    totalAmount: {
+        type: Number
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['pending', 'paid', 'failed'],
+        default: "pending"
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['cod', 'card'],
+        default: "cod"
+    },
+    orderStatus: {
+        type: String,
+        enum: ['pending', 'processing', 'shipped', 'delivered', 'cancelled'],
+        default: "pending"
+    },
+    shippingAddress: {
+        type: Object
+    },
+    createdAt: {
+        type: Date
+    }
+})
+
+export const Order = mongoose.model('orders', orderSchema);
