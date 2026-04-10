3d85a42871de9158438991d0de1d7566c874e374
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-04-10 22:46:12 +0200

    Update: checkUserDeleted middleware

--- middleware/checkUserDeleted.js
+++ middleware/checkUserDeleted.js
@@ -2,13 +2,8 @@ import { User } from "../database/model/user.model.js";
 
 export default function checkUserDeleted(req, res, next) {  
     const { id } = req.params;
-    User.findById(id).then((user) => {
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
+    User.findById(id).then(user => {
+        if (user.isDeleted) return res.status(400).json({ message: "User is deleted" });
+             next();
+            })
         }
-        if (user.isDeleted) {
-            return res.status(400).json({ message: "User is deleted" });
-        }
-        next();
-    });
-}
